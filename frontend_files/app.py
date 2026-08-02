770f136c57c9c9eeef95d6ba622ae1d2dc85c564
Nitin Sharma <nitinshm@users.noreply.github.com>
2026-08-02 15:32:41 +0530

    Updating frontend_files/app.py

--- frontend_files/app.py
+++ frontend_files/app.py
@@ -1,15 +1,13 @@
 
 import streamlit as st
 import pandas as pd
-import joblib
+import os
+import requests
 import numpy as np
 
-# Load the trained model
-@st.cache_resource
-def load_model():
-    return joblib.load("store_sales_prediction_model_v1_0.joblib")
-
-model = load_model()
+API_URL = os.environ.get("API_URL", "http://backend:7860").rstrip("/")
+API_URL_PREDICT = API_URL + "/v1/predict"
+API_URL_PREDICT_BATCH = API_URL + "/v1/predictbatch"
 
 # Streamlit UI for Price Prediction
 st.title("SmartKart Store Sales Prediction App")
@@ -30,10 +28,10 @@ Product_Type = st.selectbox("Product Type", [
 Product_MRP = st.number_input("Product MRP", min_value=0.0, step=1.0, value=150.0)
 #Store_Id = st.selectbox("Store ID", ["OUT001", "OUT002", "OUT003", "OUT004"])
 #Store_Establishment_Year = st.number_input("Store Establishment Year", min_value=1980, max_value=2025, step=1, value=2009)
-Store_Size = st.selectbox("Store Size", ["High", "Medium", "Low"])
+Store_Size = st.selectbox("Store Size", ["High", "Medium", "Small"])
 Store_Location_City_Type = st.selectbox("Store Location City Type", ["Tier 1", "Tier 2", "Tier 3"])
-Store_Type = st.selectbox("Store Type", ["Departmental Store", "Supermarket Type 1", "Supermarket Type 2", "Food Mart"])
-
+Store_Type = st.selectbox("Store Type", ["Departmental Store", "Food Mart",
+                                         "Supermarket Type1", "Supermarket Type2"])
 # Convert user input into a DataFrame
 input_data = pd.DataFrame([{
     'Product_Weight': Product_Weight,
@@ -50,5 +48,9 @@ input_data = pd.DataFrame([{
 
 # Predict button
 if st.button("Predict"):
-    prediction = model.predict(input_data)
-    st.write(f"The predicted Store sale for the product is {prediction[0]:.2f}.")
+    response = requests.post(f"{API_URL_PREDICT}", json=input_data.iloc[0].to_dict(), timeout=60)
+    if response.status_code == 200:
+        prediction = response.json()["Predicted_Store_Sales"]
+        st.write(f"The predicted Store sale for the product is {prediction:.2f}.")
+    else:
+        st.error(f"API returned {response.status_code}: {response.text}")
