ec3b56b415facb96a05c2034625eb9486a8a8153
Nitin Sharma <nitinshm@users.noreply.github.com>
2026-08-02 16:34:06 +0530

    Updating frontend_files/app.py

--- frontend_files/app.py
+++ frontend_files/app.py
@@ -5,7 +5,7 @@ import os
 import requests
 import numpy as np
 
-API_URL = os.environ.get("API_URL", "http://backend:7860").rstrip("/")
+API_URL = os.environ.get("API_URL", "http://backend:7680").rstrip("/")
 API_URL_PREDICT = API_URL + "/v1/predict"
 API_URL_PREDICT_BATCH = API_URL + "/v1/predictbatch"
 
@@ -26,8 +26,6 @@ Product_Type = st.selectbox("Product Type", [
     "household", "seafood", "starchy foods", "others"
 ])
 Product_MRP = st.number_input("Product MRP", min_value=0.0, step=1.0, value=150.0)
-#Store_Id = st.selectbox("Store ID", ["OUT001", "OUT002", "OUT003", "OUT004"])
-#Store_Establishment_Year = st.number_input("Store Establishment Year", min_value=1980, max_value=2025, step=1, value=2009)
 Store_Size = st.selectbox("Store Size", ["High", "Medium", "Small"])
 Store_Location_City_Type = st.selectbox("Store Location City Type", ["Tier 1", "Tier 2", "Tier 3"])
 Store_Type = st.selectbox("Store Type", ["Departmental Store", "Food Mart",
@@ -39,8 +37,6 @@ input_data = pd.DataFrame([{
     'Product_Allocated_Area': Product_Allocated_Area,
     'Product_Type': Product_Type,
     'Product_MRP': Product_MRP,
-    #'Store_Id': Store_Id,
-    #'Store_Establishment_Year': Store_Establishment_Year,
     'Store_Size': Store_Size,
     'Store_Location_City_Type': Store_Location_City_Type,
     'Store_Type': Store_Type
